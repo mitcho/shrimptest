4ac39ea7ce586755ca2aac671dd855c8397a3c4e
Michael Yoshitaka Erlewine <mitcho@mitcho.com>
2010-06-19 01:40:26 +0000

    - quick refactoring
    git-svn-id: http://plugins.svn.wordpress.org/shrimptest/trunk@254591 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- template-tags.php
+++ template-tags.php
@@ -23,17 +23,16 @@
 
 function shrimptest_get_variant( $experiment_id ) {
 	global $shrimp;
-	return $shrimp->get_visitor_variant( $experiment_id );
+	return $shrimp->get_visitor_variant( (int) $experiment_id );
 }
 
 /*
  * LOW LEVEL METRIC SETTING CODE:
  */
 
-// NOTE: right now $metric_id = $experiment_id. TODO: fix.
 function shrimptest_update_metric( $metric_id, $value ) {
 	global $shrimp;
-	$shrimp->update_visitor_metric( $metric_id, $value );
+	$shrimp->update_visitor_metric( (int) $metric_id, (float) $value );
 }
 
 
@@ -43,7 +42,6 @@ function shrimptest_update_metric( $metric_id, $value ) {
  */
 
 function shrimptest_conversion_success( $metric_id ) {
-	global $shrimp;
-	$shrimp->update_visitor_metric( $metric_id, 1, true );
+	shrimptest_update_metric( $metric_id, 1 );
 }
 
