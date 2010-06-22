b78f24801552f4d150fdaa46ebf9f9d5877fa527
Michael Yoshitaka Erlewine <mitcho@mitcho.com>
2010-06-22 04:11:04 +0000

    - bugfix to avoid "Call-time pass-by-reference" error
    git-svn-id: http://plugins.svn.wordpress.org/shrimptest/trunk@255754 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- blocklist.php
+++ blocklist.php
@@ -1,7 +1,3 @@
-
-Warning: Unknown: It is not safe to rely on the system's timezone settings. You are *required* to use the date.timezone setting or the date_default_timezone_set() function. In case you used any of those methods and you are still getting this warning, you most likely misspelled the timezone identifier. We selected 'America/New_York' for 'EDT/-4.0/DST' instead in /Users/mitcho/shrimptest/create-blocklist.php on line 47
-
-Deprecated: Call-time pass-by-reference has been deprecated in /Users/mitcho/shrimptest/create-blocklist.php on line 47
 <?php
 /*
  * USER AGENT BLOCKLIST
