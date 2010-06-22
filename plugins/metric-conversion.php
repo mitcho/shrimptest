eae070b72c851f1000c1b2445fa6465c74542d22
Michael Yoshitaka Erlewine <mitcho@mitcho.com>
2010-06-22 00:02:14 +0000

    + conversion save code, including title computation by query vars
    git-svn-id: http://plugins.svn.wordpress.org/shrimptest/trunk@255706 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugins/metric-conversion.php
+++ plugins/metric-conversion.php
@@ -7,7 +7,11 @@ add_action( 'shrimptest_init', 'shrimptest_metric_conversion_init', 10, 1 );
 add_action( 'shrimptest_add_metric_extra', 'shrimptest_metric_conversion_extra', 10, 1 );
 add_action( 'shrimptest_admin_header', 'shrimptest_metric_conversion_script_and_style' );
 
-// Utils
+// Filter for adjusting the aggregate value SQL expression ( max(ifnull(value,0)) )
+// add_filter( 'shrimptest_get_stats_value_conversion' )
+add_filter( 'shrimptest_save_metric_conversion', 'shrimptest_metric_conversion_save_filter' );
+
+// Utility function
 add_filter( 'wp_headers', 'shrimptest_metric_conversion_print_query_headers', 10, 2 );
 
 function shrimptest_metric_conversion_init( $shrimp ) {
@@ -47,9 +51,51 @@ input.default {color: #ccc;}
 <?php
 }
 
+function shrimptest_metric_conversion_save_filter( $metric_data ) {
+
+	// first things first, ensure that the $metric_data includes the appropriate parameters for a 
+	// conversion-style metric.
+	$metric_data['ifnull'] = true;
+	$metric_data['nullvalue'] = 0;
+	$metric_data['direction'] = 'greater'; // is better
+
+	$url = $metric_data['conversion_url'];
+	if ( empty( $url ) )
+		wp_die( __( 'No conversion URL was specified.', 'shrimptest' ) );
+	if ( stripos( $url, site_url() ) !== 0 )
+		wp_die( __( 'The specified conversion URL is not part of your WordPress site. Please go back and enter another.', 'shrimptest' ) );
+
+	// first try the post ID method. will work if this URL is_single.
+	$post_ID = url_to_postid( $url );
+	if ( $post_ID ) {
+		$title = get_the_title($post_ID);
+		$metric_data['name'] = sprintf( __("Conversion: %s (%s)",'shrimptest'), $title, $url );
+		$metric_data['conversion_post_ID'] = $post_ID;
+		return $metric_data;
+	}
+	
+	// next let's try to store the URL's query vars array.
+	$query_vars = shrimptest_metric_conversion_retrieve_query_vars( $url );
+	if ( $query_vars ) {
+		// reconstruct the appropriate title, right here, right now.
+		// NOTE: this method may be relatively fragile.
+		query_posts( $query_vars );
+		$title = wp_title( '|', false, 'right' );
+		$title = preg_replace( '/^\s*\|?\s*(.*?)\s*\|?\s*$/', '$1', $title );
+		$metric_data['name'] = sprintf( __("Conversion: %s (%s)",'shrimptest'), $title, $url );
+		$metric_data['conversion_query_vars'] = $query_vars;
+		return $metric_data;
+	}
+
+	// else, we have to kick it back to the user saying we couldn't resolve that URL.
+	wp_die( __( 'The specified conversion URL is not part of your WordPress site. Please go back and enter another.', 'shrimptest' ) );
+
+}
+
 function shrimptest_metric_conversion_print_query_headers( $headers, $this_query ) {
-	if ( isset( $_GET[ SHRIMPTEST_QUERY_VARS_PARAMETER ] ) )
+	if ( isset( $_GET[ SHRIMPTEST_QUERY_VARS_PARAMETER ] ) ) {
 		$headers[ SHRIMPTEST_QUERY_VARS_HEADER ] = serialize( $this_query->query_vars );
+	}
 	return $headers;
 }
 
@@ -67,5 +113,6 @@ function shrimptest_metric_conversion_retrieve_query_vars( $url ) {
 	if ( !isset( $headers[ strtolower( SHRIMPTEST_QUERY_VARS_HEADER ) ] ) )
 		return false;
 	else
-		return $headers[ strtolower( SHRIMPTEST_QUERY_VARS_HEADER ) ];
+		return unserialize( $headers[ strtolower( SHRIMPTEST_QUERY_VARS_HEADER ) ] );
 }
+
